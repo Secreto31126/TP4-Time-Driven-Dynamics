da2190ab95889436e7b6dc63738e87275448b63c
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-25 02:56:15 -0300

    Remove static methods from Vector3
    They actually interfere with shorthand method references

--- src/main/java/org/sims/models/Vector3.java
+++ src/main/java/org/sims/models/Vector3.java
@@ -5,75 +5,39 @@ public record Vector3(double x, double y, double z) {
     public static final Vector3 ZERO = ZERO_ZERO_ZERO;
 
     public Vector3 neg() {
-        return Vector3.neg(this);
+        return new Vector3(-x, -y, -z);
     }
 
     public Vector3 add(final Vector3 v) {
-        return Vector3.add(this, v);
+        return new Vector3(x + v.x, y + v.y, z + v.z);
     }
 
     public Vector3 subtract(final Vector3 v) {
-        return Vector3.subtract(this, v);
+        return new Vector3(x - v.x, y - v.y, z - v.z);
     }
 
     public Vector3 mult(final double scalar) {
-        return Vector3.mult(this, scalar);
+        return new Vector3(x * scalar, y * scalar, z * scalar);
     }
 
     public Vector3 div(final double scalar) {
-        return Vector3.div(this, scalar);
+        return new Vector3(x / scalar, y / scalar, z / scalar);
     }
 
     public double dot(final Vector3 v) {
-        return Vector3.dot(this, v);
+        return x * v.x + y * v.y + z * v.z;
     }
 
     public double norm() {
-        return Vector3.norm(this);
+        return Math.sqrt(this.norm2());
     }
 
     public double norm2() {
-        return Vector3.norm2(this);
+        return x * x + y * y + z * z;
     }
 
     public Vector3 hadamard(final Vector3 v) {
-        return Vector3.hadamard(this, v);
-    }
-
-    public static Vector3 neg(final Vector3 v) {
-        return new Vector3(-v.x, -v.y, -v.z);
-    }
-
-    public static Vector3 add(final Vector3 v1, final Vector3 v2) {
-        return new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
-    }
-
-    public static Vector3 subtract(final Vector3 v1, final Vector3 v2) {
-        return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
-    }
-
-    public static Vector3 mult(final Vector3 v, final double scalar) {
-        return new Vector3(v.x * scalar, v.y * scalar, v.z * scalar);
-    }
-
-    public static Vector3 div(final Vector3 v, final double scalar) {
-        return new Vector3(v.x / scalar, v.y / scalar, v.z / scalar);
-    }
-
-    public static double dot(final Vector3 v1, final Vector3 v2) {
-        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-    }
-
-    public static double norm(final Vector3 v) {
-        return Math.sqrt(Vector3.norm2(v));
-    }
-
-    public static double norm2(final Vector3 v) {
-        return v.x * v.x + v.y * v.y + v.z * v.z;
-    }
-
-    public static Vector3 hadamard(final Vector3 v1, final Vector3 v2) {
-        return new Vector3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+        return new Vector3(x * v.x, y * v.y, z * v.z);
     }
 
     @Override
