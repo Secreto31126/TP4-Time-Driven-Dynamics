bcce58f1f741e1e3484871f4460e13b29e22cbb2
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-26 20:19:17 -0300

    Add Constructor interface

--- src/main/java/org/sims/interfaces/Integrator.java
+++ src/main/java/org/sims/interfaces/Integrator.java
@@ -6,4 +6,8 @@ import org.sims.models.Particle;
 
 public interface Integrator {
     List<Particle> step(final Collection<Particle> particles);
+
+    public interface Constructor {
+        Integrator get(double dt, ForceCalculator force);
+    }
 }
