265aecb2bdd5e0f421112bc83b768df34f00d724
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-26 17:33:59 -0300

    Improve ForceCalculator signature

--- src/main/java/org/sims/interfaces/ForceCalculator.java
+++ src/main/java/org/sims/interfaces/ForceCalculator.java
@@ -1,9 +1,9 @@
 package org.sims.interfaces;
 
-import java.util.Collection;
+import java.util.*;
 import java.util.function.BiFunction;
 
 import org.sims.models.*;
 
-public interface ForceCalculator extends BiFunction<Particle, Collection<Particle>, Vector3> {
+public interface ForceCalculator extends BiFunction<Collection<Particle>, List<Double>, Map<Particle, Vector3>> {
 }
