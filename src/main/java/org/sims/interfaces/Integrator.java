91ce0b0c2059841e245e3c2be4d3c246ea144e8f
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-26 20:19:25 -0300

    Document step method

--- src/main/java/org/sims/interfaces/Integrator.java
+++ src/main/java/org/sims/interfaces/Integrator.java
@@ -5,6 +5,12 @@ import java.util.*;
 import org.sims.models.Particle;
 
 public interface Integrator {
+    /**
+     * Advance the simulation by one time step
+     *
+     * @param particles the particles to move
+     * @return the moved particles
+     */
     List<Particle> step(final Collection<Particle> particles);
 
     public interface Constructor {
