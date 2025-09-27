9191913b206caae140ad9dbfcf95d5f06066d956
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-27 03:57:14 -0300

    Ever so slightly faster

--- src/main/python/frames.py
+++ src/main/python/frames.py
@@ -1,6 +1,8 @@
 from functools import cache
 import os
 
+import pandas as pd
+
 from classes.particle import Particle
 import resources
 
@@ -9,8 +11,8 @@ def next(f: int):
     Reads the input file for a given frame.
     """
     file_path = resources.path('steps', f"{f}.txt")
-    with open(file_path, 'r') as file:
-        return f, [Particle(*map(float, line.strip().split())) for line in file]
+    df = pd.read_csv(file_path, header=None, delimiter=' ').astype("float") # type: ignore[reportUnknownMemberType]
+    return f, [Particle(*d) for _, d in df.iterrows()]
 
 @cache
 def count():
