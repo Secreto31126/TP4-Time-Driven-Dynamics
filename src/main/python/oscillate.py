24e4912dde61764a023a5b9352d9c0fb08e7f0a4
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-26 21:19:20 -0300

    Graph oscilloscope
    Co-Authored-By: Gianfranco Magliotti <95419759+gmagliotti@users.noreply.github.com>

---
+++ src/main/python/oscillate.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+import frames
+
+M = 70.0
+K = 1e4
+GAMMA = 100.0
+A = 1.0
+
+def solution(t: np.ndarray):
+    w0 = np.sqrt(K / M)
+    beta = GAMMA / (2 * M)
+    wd = np.sqrt(w0 ** 2 - beta ** 2)
+    x = A * np.exp(t * -beta) * np.cos(wd * t)
+    return x
+
+data: list[float] = []
+for i in range(frames.count()):
+    _, particles = frames.next(i)
+    p = particles[0]
+    data.append(p.position.x)
+
+t_ref = np.linspace(0, 5, frames.count())
+x_ana_curve = solution(t_ref)
+
+plt.plot(t_ref, data) # pyright: ignore[reportUnknownMemberType]
+plt.plot(t_ref, x_ana_curve) # pyright: ignore[reportUnknownMemberType]
+
+plt.xticks(fontsize=20) # pyright: ignore[reportUnknownMemberType]
+plt.yticks(fontsize=20) # pyright: ignore[reportUnknownMemberType]
+
+plt.xlabel("Tiempo (s)", fontsize=24) # pyright: ignore[reportUnknownMemberType]
+plt.ylabel("Posición (m)", fontsize=24) # pyright: ignore[reportUnknownMemberType]
+
+plt.ylim(-1.1, 1.1) # pyright: ignore[reportUnknownMemberType]
+
+plt.show() # pyright: ignore[reportUnknownMemberType]
