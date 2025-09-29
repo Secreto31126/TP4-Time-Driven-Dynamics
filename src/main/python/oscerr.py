4f3812ceefae7d8853ac65b8cb9eba31aad2636b
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-29 03:11:32 -0300

    Graph oscillator errors
    For some reason it looks broken tho...

---
+++ src/main/python/oscerr.py
@@ -0,0 +1,37 @@
+import os
+
+import matplotlib.pyplot as plt
+
+import resources
+
+values: dict[str, tuple[list[float], list[float]]] = {}
+
+errors = resources.path("osc-error")
+for integral in os.listdir(errors):
+    integral_path = resources.path("osc-error", integral)
+
+    for filename in os.listdir(integral_path):
+        dt_path = os.path.join(integral_path, filename)
+
+        dt = filename[:-4]
+        with open(dt_path, "r") as f:
+            err = float(f.readline().strip())
+
+        if integral not in values:
+            values[integral] = ([], [])
+        values[integral][0].append(float(dt))
+        values[integral][1].append(err)
+
+for integral, (dts, errs) in values.items():
+    sorted_pairs = sorted(zip(dts, errs))
+    dts, errs = zip(*sorted_pairs)
+
+    plt.loglog(dts, errs, marker='o', label=integral) # pyright: ignore[reportUnknownMemberType]
+
+plt.xticks(fontsize=20) # pyright: ignore[reportUnknownMemberType]
+plt.yticks(fontsize=20) # pyright: ignore[reportUnknownMemberType]
+
+plt.xlabel("dt (s)", fontsize=24) # pyright: ignore[reportUnknownMemberType]
+plt.ylabel("Error Cuadrático Medio", fontsize=24) # pyright: ignore[reportUnknownMemberType]
+
+plt.show() # pyright: ignore[reportUnknownMemberType]
