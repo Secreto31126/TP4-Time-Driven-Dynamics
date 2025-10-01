8a5dfa540afbfad452cf97d9b9809f34bfd7e064
julianBogado1 <jbogado@itba.edu.ar>
2025-10-01 10:42:08 -0300

    oscillator main refactor

--- src/main/java/org/sims/MainOscillator.java
+++ src/main/java/org/sims/MainOscillator.java
@@ -2,32 +2,112 @@ package org.sims;
 
 import org.sims.integrals.GearPositionIntegrator;
 import org.sims.interfaces.Force;
+import org.sims.interfaces.Integrator;
 import org.sims.models.Particle;
+import org.sims.models.Vector3;
 import org.sims.oscillator.*;
 
 import me.tongfei.progressbar.ProgressBar;
 
-/**
- * If you are boring and don't want to use jdk25
- *
- * So much boilerplate 🤢
- */
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainOscillator {
     private static final long SAVE_INTERVAL = 10L;
 
     public static void main(final String[] args) throws Exception {
-        final var seconds = 5.0;
-        final var dt = Double.parseDouble(args[1]);
+        final double dt = Double.parseDouble(args[1]);
+        final String integrator = args[0];
+        //TODO parse from json
+        new Simulator(dt, integrator, 1.0, 0.2, 1.0).simulate();
+    }
 
-//        final var integrator = IntegratorPicker.pick(args[0]);
-        final var integrator = new GearPositionIntegrator.Constructor();
-        final var simulation = OscillatorSimulation.build((long) (seconds / dt), dt, 1e4, 100, 70.0, integrator);
+}
 
-        final var pb = new ProgressBar("Oscillating", simulation.steps());
-        final var onStep = new Orchestrator.SkipSteps(SAVE_INTERVAL, pb::step);
+class Simulator{
+    public final double dt;
+    public final long steps;
+    public String integrationMethod;
+    public final OscillatorForce force;
+    public static final long SAVE_INTERVAL = 10L;
 
-        try (pb; final var engine = new OscillatorEngine(simulation)) {
-            new Orchestrator(simulation, engine).start(onStep);
+    public Simulator(final double dt, final String integrationMethod, Double springConstant, Double dampingCoefficient, Double mass) {
+        this.dt = dt;
+        this.integrationMethod = integrationMethod;
+        this.force = new OscillatorForce(springConstant, dampingCoefficient, mass);
+        this.steps = (long) (5.0 / dt);
+    }
+
+    public void simulate() throws IOException {
+        Integrator<Particle> integrator = null;
+        List<Particle> particles = List.of(new Particle(new Vector3(1,0,0), Vector3.ZERO, 1.0));
+        String integratorName = "";
+
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        Object lock = new Object();
+        switch(integrationMethod){
+            case "gearposition":
+                integrator = new GearPositionIntegrator(dt, force);
+                integratorName = "Gear Position";
+                break;
+            case "gearvelocity":
+                //TODO
+                break;
+            case "verlet":
+                //TODO
+                break;
+            default:
+                System.out.println("Unknown integration method: " + integrationMethod);
+                System.out.println("Available methods: gearposition, gearvelocity, verlet");
+                System.exit(1);
+        }
+
+        File file = new File("steps/output.txt");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(String.format(Locale.US,"%d %.14f %.14f %.14f %.14f %s\n",
+                steps, dt, force.k(), force.gamma(), force.mass(), integratorName));
+        writer.close();
+
+        for(int i=0; i < steps; i++){
+            //1. Compute next step
+            particles = integrator.step(particles); //TODO remove warning
+
+            //2. Save to file every SAVE_INTERVAL steps
+            if(i%SAVE_INTERVAL == 0){
+                executor.submit(new SaveIntegrationStep((int) (i/SAVE_INTERVAL), lock, particles));
+            }
+        }
+
+    }
+
+}
+
+class SaveIntegrationStep implements Runnable {
+    private final Object lock;
+    private final List<Particle> particles;
+    File file;
+
+    public SaveIntegrationStep(int fileIndex, Object lock, List<Particle> particles) throws IOException {
+        this.lock = lock;
+        this.particles = particles;
+        file = new File("steps/"+ fileIndex+".txt");
+    }
+
+    @Override
+    public void run() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (Particle particle : particles) {
+                writer.write(String.format("%s%n", particle));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
+
