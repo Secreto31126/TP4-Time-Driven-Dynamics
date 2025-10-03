a8dcdafa8a53e27d2b70aa040c92b225ecbe7e3d
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-10-03 04:29:11 -0300

    Add HMR graph

--- src/main/python/ener.py
+++ src/main/python/ener.py
@@ -29,3 +29,29 @@ def potential(particles: list[Particle], h: float):
             e += - 1 / math.sqrt(r.norm2() + H2)
 
     return e
+
+def center_of_mass(particles: list[Particle]):
+    """
+    Center of mass of a list of particles.
+
+    Assumes mass == 1 for each particle.
+    """
+    n = float(len(particles))
+    pos = reduce(lambda acc, p: acc + p.position, particles, particles[0].position * 0)
+    return pos / n
+
+def half_mass_radius(particles: list[Particle]):
+    """
+    Half-mass radius of a list of particles.
+
+    Assumes mass == 1 for each particle.
+    """
+    com = center_of_mass(particles)
+
+    distances = sorted([(p.position - com).norm() for p in particles])
+
+    half_index = len(distances) // 2
+    if len(distances) % 2 == 0:
+        return (distances[half_index - 1] + distances[half_index]) / 2
+    else:
+        return distances[half_index]
