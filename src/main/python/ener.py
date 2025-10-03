75ab91d9b52e85d1baa77e4fbca6c378865fb7ac
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-10-03 04:25:50 -0300

    Fix ener.py methods

--- src/main/python/ener.py
+++ src/main/python/ener.py
@@ -12,7 +12,7 @@ def kinetic(particles: list[Particle]):
 
     Assumes mass == 1 for each particle.
     """
-    return reduce(lambda acc, p: acc + p.velocity.norm() / 2, particles, 0.0)
+    return reduce(lambda acc, p: acc + p.velocity.norm2() / 2, particles, 0.0)
 
 def potential(particles: list[Particle], h: float):
     """
@@ -26,6 +26,6 @@ def potential(particles: list[Particle], h: float):
     for i, p in enumerate(particles):
         for o in particles[i+1:]:
             r = p.position - o.position
-            e += - 1 / math.sqrt(r.norm() + H2)
+            e += - 1 / math.sqrt(r.norm2() + H2)
 
     return e
