2332db88f82e29d8b14c61aeb31c0ce94167c1f2
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-10-04 19:39:16 -0300

    Allow custom input directories

--- src/main/python/frames.py
+++ src/main/python/frames.py
@@ -6,17 +6,17 @@ import pandas as pd
 from classes.particle import Particle
 import resources
 
-def next(f: int):
+def next(f: int, dir: str = 'steps'):
     """
     Reads the input file for a given frame.
     """
-    file_path = resources.path('steps', f"{f}.txt")
+    file_path = resources.path(dir, f"{f}.txt")
     df = pd.read_csv(file_path, header=None, delimiter=' ').astype("float") # type: ignore[reportUnknownMemberType]
     return f, [Particle(*d) for _, d in df.iterrows()]
 
 @cache
-def count():
+def count(dir: str = 'steps'):
     """
     Returns the number of animations steps.
     """
-    return len(os.listdir(resources.path('steps')))
+    return len(os.listdir(resources.path(dir)))
