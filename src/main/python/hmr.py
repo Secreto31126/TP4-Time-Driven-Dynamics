55dc57b3afd5159c487d92173b05354a2545b31f
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-10-05 07:54:14 -0300

    Save t* in function of N, not dt

--- src/main/python/hmr.py
+++ src/main/python/hmr.py
@@ -1,9 +1,14 @@
-import os
 import matplotlib.pyplot as plt
 
 import numpy as np
 
 from tqdm import tqdm
+
+import os
+
+import sys
+
+import time
 
 import ener
 import frames
@@ -30,35 +35,38 @@ def main():
                 threshold = (i - 1) * 10
             break
 
-    return hmr, threshold, np.linspace(0, steps, frames.count()), dt, integral
+    return hmr, threshold, np.linspace(0, steps, frames.count()), integral, dt, len(frames.next(0)[1])
 
 if __name__ == "__main__":
-    hmr, threshold, steps, dt, integral = main()
-
-    plt.plot(steps, hmr) # pyright: ignore[reportUnknownMemberType]
+    hmr, threshold, steps, integral, dt, N = main()
 
     if threshold is not None:
         plt.axvline(threshold, linestyle='--', color='r') # pyright: ignore[reportUnknownMemberType]
 
-        folder = resources.path('t-star', integral)
+        folder = resources.path('t-star', integral, str(N))
         os.makedirs(folder, exist_ok=True)
-        with open(resources.path(folder, f'{dt}.txt'), 'w') as f:
-            f.write(f"{threshold}\n")
+        with open(resources.path(folder, f'{int(time.time())}.txt'), 'w') as f:
+            f.write(f"{threshold * dt}\n")
     else:
         print("No se alcanzó un t* con r < 1.")
+
+    if "--no-plot" in sys.argv:
+        exit(0)
+
+    plt.plot(steps, hmr) # pyright: ignore[reportUnknownMemberType]
 
     plt.xticks(fontsize=20) # pyright: ignore[reportUnknownMemberType]
     plt.yticks(fontsize=20) # pyright: ignore[reportUnknownMemberType]
 
     plt.xlabel("Pasos", fontsize=24) # pyright: ignore[reportUnknownMemberType]
-    plt.ylabel("Radio de masa media (m)", fontsize=24) # pyright: ignore[reportUnknownMemberType]
+    plt.ylabel(r"$r_{hm}$ (m)", fontsize=24) # pyright: ignore[reportUnknownMemberType]
 
     plt.subplots_adjust(top=0.99, right=0.99, bottom=0.1, left=0.06)
     plt.show() # pyright: ignore[reportUnknownMemberType]
 
-    n = input("Paso del estacionario (en blanco para no guardar): ")
-    if n.strip() != "":
-        hmr_est = int(n) / 10
+    i = input("Paso del estacionario (en blanco para no guardar): ")
+    if i.strip() != "":
+        hmr_est = int(i) / 10
         folder = resources.path('hmr', integral)
         os.makedirs(folder, exist_ok=True)
-        np.savetxt(resources.path(folder, f'{dt}.txt'), hmr[hmr_est:])
+        np.savetxt(resources.path(folder, f'{N}.txt'), hmr[hmr_est:])
