54d4744c4dea6cd54b7a9aeb09c33daf00cb7440
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-10-07 17:59:20 -0300

    Add cache hits

--- src/main/python/gala.py
+++ src/main/python/gala.py
@@ -21,8 +21,6 @@ from streaming import SequentialStreamingExecutor as Executor
 
 H = 0.05
 
-def main():
-    executor = Executor(frames.next, range(frames.count()))
 def truncate_at_most_2(n: float) -> str:
     TRUNC = int(n * 100) / 100
     return str(TRUNC).rstrip('0').rstrip('.')
@@ -36,22 +34,45 @@ def sci_notation(val: float, _):
 
     return f"${truncate_at_most_2(COEFF)}\\times 10^{{{EXP}}}$"
 
+def main(cache_key: str | None = None):
     with open(resources.path("setup.txt"), "r") as f:
         line = f.readline().strip().split(' ')
         steps = int(line[0])
         dt = float(line[1])
         integral = line[-1]
 
-    kin = np.array([])
-    pot = np.array([])
-    for particles in tqdm(executor.stream(), total=frames.count()):
-        kin = np.append(kin, ener.kinetic(particles))
-        pot = np.append(pot, ener.potential(particles, H))
+    if cache_key:
+        folder = resources.path('cache-galapy')
+        kin = np.load(resources.path(folder, f"{cache_key}-kin.npy"))
+        pot = np.load(resources.path(folder, f"{cache_key}-pot.npy"))
+    else:
+        executor = Executor(frames.next, range(frames.count()))
 
-    return kin, pot, kin + pot, np.linspace(0, steps, frames.count()) * dt, dt, integral, len(frames.next(0)[1])
+        kin = np.array([])
+        pot = np.array([])
+
+        for particles in tqdm(executor.stream(), total=frames.count()):
+            kin = np.append(kin, ener.kinetic(particles))
+            pot = np.append(pot, ener.potential(particles, H))
+
+    return kin, pot, kin + pot, np.linspace(0, steps * dt, frames.count()), dt, integral, len(frames.next(0)[1])
 
 if __name__ == "__main__":
-    kin, pot, tot, steps, dt, integral, n = main()
+    cache_key = next(filter(lambda x: x.startswith("--cache="), sys.argv), None)
+    if cache_key:
+        cache_key = "=".join(cache_key.split('=')[1:])
+
+    kin, pot, tot, steps, dt, integral, n = main(cache_key)
+
+    if "--cache" in sys.argv:
+        folder = resources.path('cache-galapy')
+        os.makedirs(folder, exist_ok=True)
+
+        cache_key = str(int(time.time()))
+        np.save(resources.path(folder, f"{cache_key}-kin"), kin)
+        np.save(resources.path(folder, f"{cache_key}-pot"), pot)
+
+        print(f"Cached with key: {cache_key}")
 
     if "--no-save" in sys.argv:
         folder = resources.path('graverr', integral, str(n), str(dt))
