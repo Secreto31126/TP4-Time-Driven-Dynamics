3b728a1473c9dd69263e42e9d51d2c54d2125112
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-10-08 22:20:37 -0300

    Graph fix

--- src/main/python/hmr.py
+++ src/main/python/hmr.py
@@ -35,7 +35,7 @@ def main():
                 tstar = (i - 1) * 10
             break
 
-    return hmr, tstar, np.linspace(0, steps / dt, frames.count()), integral, dt, len(frames.next(0)[1])
+    return hmr, tstar, np.linspace(0, steps * dt, frames.count()), integral, dt, len(frames.next(0)[1])
 
 if __name__ == "__main__":
     hmr, tstar, steps, integral, dt, N = main()
